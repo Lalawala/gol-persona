703380cf1e883894b68b0362d6c907bd1da7cdee
Lalawala <126595973+Lalawala@users.noreply.github.com>
2024-11-04 15:38:37 +0000

    Update gol.go

--- gol-skeleton-master/gol/gol.go
+++ gol-skeleton-master/gol/gol.go
@@ -15,13 +15,15 @@ func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 
 	ioCommand := make(chan ioCommand)
 	ioIdle := make(chan bool)
-
+	ioInput := make(chan uint8)
+	ioOutput := make(chan uint8)
+	ioFilename := make(chan string)
 	ioChannels := ioChannels{
 		command:  ioCommand,
 		idle:     ioIdle,
-		filename: nil,
-		output:   nil,
-		input:    nil,
+		filename: ioFilename,
+		output:   ioOutput,
+		input:    ioInput,
 	}
 	go startIo(p, ioChannels)
 
@@ -29,9 +31,10 @@ func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 		events:     events,
 		ioCommand:  ioCommand,
 		ioIdle:     ioIdle,
-		ioFilename: nil,
-		ioOutput:   nil,
-		ioInput:    nil,
+		ioFilename: ioFilename,
+		ioOutput:   ioOutput,
+		ioInput:    ioInput,
+		key:        keyPresses,
 	}
 	distributor(p, distributorChannels)
 }
